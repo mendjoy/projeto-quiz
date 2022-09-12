5af82f2d55a7adfffdc81cb61d747238a42c2282
Joyce M Miranda <106771419+mendjoy@users.noreply.github.com>
2022-09-12 18:51:36 -0300

    finish

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-const question = document.querySelector('#question');
-const answerBox = document.querySelector('#answerbox');
-const quizContainer = document.querySelector('#quizcontainer');
-const scoreContainer = document.querySelector('#scorecontainer');
-const letter = ['A', 'B', 'C', 'D'];
+const question = document.querySelector('#question'); 
+const answerBox = document.querySelector('#answerbox'); 
+const quizContainer = document.querySelector('#quizcontainer'); 
+const scoreContainer = document.querySelector('#scoretela'); 
+const letters = ['A', 'B', 'C', 'D'];
 let points = 0;
 let actualQuestion =0;
 
@@ -64,7 +64,7 @@ const easyQuestions = [
                 'correct': false //true or false B  
             },
             {
-                'answer': 'is - are',
+                'answer': 'are - are',
                 'correct': false //true or false C
             },
             {
@@ -97,133 +97,133 @@ const easyQuestions = [
     }, 
     {
         // question 5
-        'question': 'Today the weather is great, but last week it ____ awful.',
+        'question': `What's the plural of “man“?.`,
         'answers': [
             {
-                'answer': 'were',
+                'answer': 'mans',
                 'correct': false //true or false A
             },
             {
-                'answer': 'will be',
-                'correct': false //true or false B  
+                'answer': 'men',
+                'correct': true //true or false B  
             },
             {
-                'answer': 'has be',
+                'answer': 'mains',
                 'correct': false //true or false C
             },
             {
-                'answer': 'was',
-                'correct': true//true or false D
+                'answer': 'means',
+                'correct': false//true or false D
             }, 
         ]
     }, 
     {
         // question 6
-        'question': 'Today the weather is great, but last week it ____ awful.',
+        'question': "Which’s the first letter of the alphabet?",
         'answers': [
             {
-                'answer': 'were',
+                'answer': 'C',
                 'correct': false //true or false A
             },
             {
-                'answer': 'will be',
+                'answer': 'Z',
                 'correct': false //true or false B  
             },
             {
-                'answer': 'has be',
-                'correct': false //true or false C
+                'answer': 'A',
+                'correct': true //true or false C
             },
             {
-                'answer': 'was',
-                'correct': true//true or false D
+                'answer': 'P',
+                'correct': false//true or false D
             }, 
         ]
     }, 
     {
         // question 7
-        'question': 'Today the weather is great, but last week it ____ awful.',
+        'question': 'What do we say to people between about 6 a.m and 12 p.m?.',
         'answers': [
             {
-                'answer': 'were',
-                'correct': false //true or false A
+                'answer': 'Good morning!',
+                'correct': true //true or false A
             },
             {
-                'answer': 'will be',
+                'answer': 'Good afternon!',
                 'correct': false //true or false B  
             },
             {
-                'answer': 'has be',
+                'answer': 'Good evening!',
                 'correct': false //true or false C
             },
             {
-                'answer': 'was',
-                'correct': true//true or false D
+                'answer': 'Good night!',
+                'correct': false//true or false D
             }, 
         ]
     }, 
     {
         // question 8
-        'question': 'Today the weather is great, but last week it ____ awful.',
+        'question': `What's the plural of “a”?.`,
         'answers': [
             {
-                'answer': 'were',
+                'answer': 'Any',
                 'correct': false //true or false A
             },
             {
-                'answer': 'will be',
+                'answer': 'as',
                 'correct': false //true or false B  
             },
             {
-                'answer': 'has be',
-                'correct': false //true or false C
+                'answer': 'some',
+                'correct': true //true or false C
             },
             {
-                'answer': 'was',
-                'correct': true//true or false D
+                'answer': 'its',
+                'correct': false//true or false D
             }, 
         ]
     },
     {
         // question 9
-        'question': 'Today the weather is great, but last week it ____ awful.',
+        'question': "What's the negative of “he speaks“?.",
         'answers': [
             {
-                'answer': 'were',
+                'answer': 'he speaks a lot',
                 'correct': false //true or false A
             },
             {
-                'answer': 'will be',
+                'answer':'he not speaks',
                 'correct': false //true or false B  
             },
             {
-                'answer': 'has be',
+                'answer': 'he do not speak',
                 'correct': false //true or false C
             },
             {
-                'answer': 'was',
+                'answer': `he doesn't speak`,
                 'correct': true//true or false D
             }, 
         ]
     },  
     {
         // question 10
-        'question': 'Today the weather is great, but last week it ____ awful.',
+        'question':`what's the negative of "can"?.`,
         'answers': [
             {
-                'answer': 'were',
-                'correct': false //true or false A
+                'answer': 'cannot',
+                'correct': true //true or false A
             },
             {
-                'answer': 'will be',
+                'answer': 'cants not',
                 'correct': false //true or false B  
             },
             {
-                'answer': 'has be',
+                'answer': 'not can',
                 'correct': false //true or false C
             },
             {
-                'answer': 'was',
-                'correct': true//true or false D
+                'answer': "can's not",
+                'correct': false//true or false D
             }, 
         ]
     }, 
@@ -231,50 +231,129 @@ const easyQuestions = [
 
 ]
    
-
+//iniciar o jogo
 function startEasyQuiz(){
 
     createEasyQuestion(0)
 }
 
 function createEasyQuestion(i){
-    //limpar botões
-    const oldButtons = answers.querySelectorAll('button');
+
+    //limpa a questão para poder inserir uma nova
+    const  oldButtons = answerBox.querySelectorAll('button');
     oldButtons.forEach(function(btn){
         btn.remove();
-    }); 
-    //proxima pergunta
-    const questionText = question.querySelector('#questiontext');
-    const numberQuestion = question.querySelector('#questionnumber');
+    });
 
-    questionText.textContent = easyQuestions[i].question;
-    numberQuestion.textContent = i + 1; 
+    //alterar texto da pergunta
+     const questionText = question.querySelector('#questiontext');
+     const questionNumber = question.querySelector('#questionnumber');
 
-    //inserir alternativas
-    easyQuestions[i].answers.forEach(function( answer, i){
-        const answerTemplate = document.querySelector('.answerres').cloneNode(true);
+     questionText.textContent = easyQuestions[i].question; //verificar 
+     questionNumber.textContent =  i + 1;
 
-        const leterBtn = answerTemplate.querySelector('.btnletter');
-        const answerText = answerTemplate.querySelector('.questionanswer');
+     //exibir alternativas
+     easyQuestions[i].answers.forEach(function(answer, i){ //verificar
 
-        leterBtn.textContent = letter[i];
-        answer.textContent =  answer['answer'];
+        const answerTemplate = document.querySelector('.answer-template').cloneNode(true);
+        
+         const letterbtn = answerTemplate.querySelector('.btnletter');
+         const answerText = answerTemplate.querySelector('.questionanswer');
 
-        answerTemplate.setAttribute('correct-answer', answer['correct']);
+         letterbtn.textContent = letters[i];
+         answerText.textContent = answer['answer'];
 
+         answerTemplate.setAttribute('correctanswer', answer['correct']);
 
-        //removendo a classe hide
-        answerTemplate.classList.remove('hide');
+         //removendo hide  e template class
 
-        //mostrar as alternativas
-        answerBox.appendChild(answerTemplate);
+         answerTemplate.classList.remove('hide');
+         answerTemplate.classList.remove('answer-template');
 
-        //inserindo evento no botão
-        answerTemplate.addEventListener('click', function(){
-            console.log(this)
-        }); 
-    }); 
-    actualQuestion++;
+         //inserindo alternativas
+         answerBox.appendChild(answerTemplate);
+
+         //evento de clique nos botoes
+         answerTemplate.addEventListener('click', function(){
+            checkAnswer(this)
+         });
+     });
+
+     //incrementando numero da questão
+     actualQuestion++; 
+};
+
+//checando alternativa
+
+function checkAnswer(btn){
+    console.log(btn);
+
+    const buttons = answerBox.querySelectorAll('button');
+    buttons.forEach(function(button){
+        if(button.getAttribute('correctanswer') === 'true'){
+            button.classList.add('correctanswer');
+        //checar se usuario acertou a pergunta
+            if (btn === button){
+                points++;
+            }
+        } else{
+            button.classList.add('wronganswer')
+
+        }
+    });
+
+    //mostrar a proxima pegunta
+
+    nextQuestion()
+}; 
+
+function nextQuestion(){
+    //timer
+    setTimeout(function(){
+        //verificar se ainda há perguntas
+        if(actualQuestion >= easyQuestions.length){
+            showSucccessMessage();
+            return;
+        }; 
+        createEasyQuestion(actualQuestion); 
+    }, 700);
 }
+//tela final
+function showSucccessMessage(){
+
+    hideOrShowQuiz(); 
+
+    //trocar os dados da tela final 
+    const score = ((points / easyQuestions.length) * 100).toFixed(1);
+
+    const displayScore = document.querySelector('#displayscore span')
+    displayScore.textContent = score.toString();
+    //alterar o numero 
+    const correctAnwers = document.querySelector('#correctanswer');
+
+    correctAnwers.textContent= points;
+
+    //alterar total de perguntas
+    const totalQuestions = document.querySelector('#questionsAll');
+    totalQuestions.textContent = easyQuestions.length;
+
+};
+
+ function hideOrShowQuiz(){
+    quizContainer.classList.toggle('hide');
+    scoreContainer.classList.toggle('hide');
+ }; 
+
+ //reiniciar quiz
+ const restartBtn = document.querySelector('#restart');
+
+ restartBtn.addEventListener('click', function(){
+    //zerar o jogo
+    actualQuestion = 0;
+    points = 0;
+    hideOrShowQuiz();
+    startEasyQuiz();
+ })
+
 
 startEasyQuiz()
